04a2e6abf5a08f875e859cc4c42511a376d7c83b
Pushpendra singh <pankaj182007@gmail.com>
2024-07-18 16:24:07 +0530

    implimented Callbacks, Promises & Async Await

--- scriptCallbacksPromisesAsyncAwait.js
+++ scriptCallbacksPromisesAsyncAwait.js
@@ -46,14 +46,87 @@ let promise=new Promise((resolve,reject)=>{
 const getPromise=()=>{
     return new Promise((resolve,reject)=>
     {
-     console.log("I am Promise");
-     resolve("Success");   
+        setTimeout(()=>{
+            console.log("I am Promise");
+            resolve("Success");
+           // reject("error");   
+        },3000)
+     
+     
     });
 }
 //now calling promise
 let promise1=getPromise();
 // .then is sued when promise is resolved or fullfil
-//.catch is used when promise is rejected
-promise1.then(()=>{
-    console.log("promise resolved");
+promise1.then((res)=>{
+    console.log("promise resolved",res);
 });
+
+//.catch is used when promise is rejected
+promise1.catch((err)=>{
+    console.log("promise rejected",err);
+});
+
+//Now appling promise chaining
+//create a promise
+function geDataPro(dataId)
+{
+return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        console.log("Chaining Data = ",dataId);
+        resolve("Success");
+    },3000);
+});
+}
+
+//now doing promise chaining
+geDataPro(1)
+.then((res)=>{
+    return geDataPro(2);
+})
+.then((res)=>{
+    console.log("final msg ",res);
+});
+
+//Appling Async Await
+
+function getOutPut()
+{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("Weather data");
+            resolve("Success");
+        },3000);
+        
+    });
+}
+//using Async-Await
+async function weatherApi(){
+    await getOutPut();
+}
+weatherApi();
+
+//IIFE Immediately Invoked Function Expression
+//it is a fumction that is called immediately as soon as it is defined
+//there are 3 ways to defined it
+// 1.
+// (function()
+// {
+//     ......
+// })();
+
+// 2.
+// (()=>{
+//     ......
+// })();
+
+// 3.
+// (async()=>{
+//     .....
+// })();
+
+//now write IIFE of async function
+(async()=>
+{
+    await getOutPut();
+})();
